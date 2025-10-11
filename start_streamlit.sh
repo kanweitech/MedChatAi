0924cf649287a0bad22c26d65d027a3af16690c5
kanweitech <kanweiedward@gmail.com>
2025-10-10 17:29:07 -0700

    Corrected start_streamlit.sh file

--- start_streamlit.sh
+++ start_streamlit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "🚀 Starting Streamlit frontend..."
 # Streamlit automatically uses $PORT from Render
-streamlit run app/frontend/streamlit_app.py --server.port $PORT --server.address 0.0.0.0
+streamlit run frontend/streamlit_app.py --server.port $PORT --server.address 0.0.0.0
